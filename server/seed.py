f24b9bbbe8e5f4dc78d84a2102f6b31e8fc108d0
Sharon Moegi <sharamoegi@gmail.com>
2026-07-27 22:14:31 +0300

    Clear out Tables

--- server/seed.py
+++ server/seed.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
 from app import app
-from models import *
+from models import db, Exercise, Workout, WorkoutExercises
+from datetime import date
 
 with app.app_context():
 
 	# reset data and add new example data, committing to db
+    #clear tables
+    WorkoutExercises.query.delete()
+    Workout.query.delete()
+    Exercise.query.delete()
